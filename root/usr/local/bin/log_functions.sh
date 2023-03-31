43669c0155df88f28df86172ca48ff8e93d43628
Stuart Thomas <Stuart.Thomas@fred.com.au>
2023-04-01 10:17:55 +1100

    fixed typo

--- root/usr/local/bin/log_functions.sh
+++ root/usr/local/bin/log_functions.sh
@@ -7,7 +7,7 @@ logf() {
         log_message="$(date "$(printenv DATE_FORMAT)") $1: $2"
     fi
     echo $log_message
-    if [ -z "$3" ]; then
+    if ! [ -z "$3" ]; then
         echo $log_message >> $3
     fi
 }
