17c937346dd58e17b4211f151530a74b46dd4ec2
Stuart Thomas <Stuart.Thomas@fred.com.au>
2023-04-01 14:40:58 +1100

    fixed error logging

--- root/usr/local/bin/log_functions.sh
+++ root/usr/local/bin/log_functions.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/with-contenv bash
 
 logf() {
-    if [ -z "$2" ]; then
-        log_message="$(date "$(printenv DATE_FORMAT)") INC_FOLDER_SYNC: $1"
+    message_or_tag="$1"
+    message="$2"
+    log_file="$3"
+    if [ -z "$message_tag" ]; then
+        log_message="$(date "$(printenv DATE_FORMAT)") INC_FOLDER_SYNC: $message_or_tag"
     else
-        log_message="$(date "$(printenv DATE_FORMAT)") $1: $2"
+        log_message="$(date "$(printenv DATE_FORMAT)") $message_or_tag: $message"
     fi
     echo $log_message
-    if ! [ -z "$3" ]; then
-        echo "$log_message" >> "$3"
+    if ! [ -z "$log_file" ]; then
+        echo "$log_message" >> "$log_file"
     fi
 }
 
 errorf() {
-    logf " ERROR: $1: $2"
+    message_or_tag="$1"
+    message="$2"
+    log_file="$3"
+    logf "ERROR: $message_or_tag" "$message" "$log_file"
 }
